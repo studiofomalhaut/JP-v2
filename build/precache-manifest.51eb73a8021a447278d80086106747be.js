44651df926f2ad94daf58c7cec1e70639f8d6669
Yana Naidenov <y.naidenov@gmail.com>
2019-01-24 18:42:38 +0100

    site upload

---
+++ build/precache-manifest.51eb73a8021a447278d80086106747be.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f3664da1a3310b27f48",
+    "url": "/static/css/main.c1de9cf3.chunk.css"
+  },
+  {
+    "revision": "4f3664da1a3310b27f48",
+    "url": "/static/js/main.4f3664da.chunk.js"
+  },
+  {
+    "revision": "161afabb9946b5855405",
+    "url": "/static/js/1.161afabb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1bbdbcbc5a36501a47f5ce05222b6f6",
+    "url": "/static/media/tables.f1bbdbcb.jpg"
+  },
+  {
+    "revision": "06f20acb2771958f6dd5290a6720bd4f",
+    "url": "/static/media/salaogrande-fora.06f20acb.jpg"
+  },
+  {
+    "revision": "e24b0af3ea10638bb88ca765468d21c3",
+    "url": "/static/media/pqinfant.e24b0af3.jpg"
+  },
+  {
+    "revision": "3dd8d36198760c108ff5ceea91935041",
+    "url": "/static/media/rabbit.3dd8d361.jpg"
+  },
+  {
+    "revision": "b13ff8e5f371ee443048e91129be3043",
+    "url": "/static/media/salaconf-dentro-planos.b13ff8e5.jpg"
+  },
+  {
+    "revision": "111489fbbf91da08aaddd809a5e64128",
+    "url": "/static/media/gallery8.111489fb.jpg"
+  },
+  {
+    "revision": "2efc6575245c7edee4d18d2988f6a8a0",
+    "url": "/static/media/batik.2efc6575.jpg"
+  },
+  {
+    "revision": "8e4570af8a097af902261a019afba03b",
+    "url": "/static/media/jpcopy.8e4570af.png"
+  },
+  {
+    "revision": "b815085194173201d644a89ecac6ec9a",
+    "url": "/static/media/jp-logo-small.b8150851.png"
+  },
+  {
+    "revision": "b695e7ba3202b37cd21cd3613a2915eb",
+    "url": "/static/media/rustico-prof.b695e7ba.jpg"
+  },
+  {
+    "revision": "4f8babd014a22a1fb3940030acb0bcc2",
+    "url": "/static/media/tree.4f8babd0.jpg"
+  },
+  {
+    "revision": "565a090015b7432b2a752cb32ab713a6",
+    "url": "/static/media/wedding-cake.565a0900.jpg"
+  },
+  {
+    "revision": "f2bfc0f888417d57269cd0d28dd06ef2",
+    "url": "/static/media/gazebo2.f2bfc0f8.jpeg"
+  },
+  {
+    "revision": "7613960ae1893c4fe1ab1fb148118664",
+    "url": "/static/media/gallery5.7613960a.jpg"
+  },
+  {
+    "revision": "7be8cc9f8dfd79e2c4ebd0bf769da1f3",
+    "url": "/static/media/parqueinf.7be8cc9f.jpg"
+  },
+  {
+    "revision": "a29ce70eb8636145d0496b46a39ad7f9",
+    "url": "/static/media/bttrfly.a29ce70e.jpg"
+  },
+  {
+    "revision": "df7bd79bd9cc3e92750421a3fc201d91",
+    "url": "/static/media/rustico-sm.df7bd79b.jpg"
+  },
+  {
+    "revision": "6b81239f396ef24726c83ff447b78a78",
+    "url": "/static/media/piscinas-sm.6b81239f.jpg"
+  },
+  {
+    "revision": "0de76a7acc82842fa9f92cf099596569",
+    "url": "/static/media/gallery3.0de76a7a.jpg"
+  },
+  {
+    "revision": "a15bcfc5dd9b65f938912452ce8f5d12",
+    "url": "/static/media/gallery6.a15bcfc5.jpg"
+  },
+  {
+    "revision": "a20e3c80c2b482745b5c2192ebd792be",
+    "url": "/static/media/gallery11.a20e3c80.jpg"
+  },
+  {
+    "revision": "6bd3cabf270ca2be3fff17e7a443dbc2",
+    "url": "/static/media/parking.6bd3cabf.jpg"
+  },
+  {
+    "revision": "35d0e5895e186f7d1a81f1c5a19b23de",
+    "url": "/static/media/parking2.35d0e589.jpg"
+  },
+  {
+    "revision": "ed3b9521a5a7579f4643afb2ab707dfe",
+    "url": "/static/media/bar.ed3b9521.jpg"
+  },
+  {
+    "revision": "8ac20924e9b533e39716c7c06d982c6c",
+    "url": "/static/media/bar-fora.8ac20924.jpg"
+  },
+  {
+    "revision": "53f1bb4672b6d3f9e884dff0c2e933c1",
+    "url": "/static/media/wc.53f1bb46.jpg"
+  },
+  {
+    "revision": "8b1452750b916c2e3c424fa483ddcb06",
+    "url": "/static/media/gazebo-old-sm.8b145275.jpg"
+  },
+  {
+    "revision": "d2d8f5bec7a849dafc2eeec4c3494316",
+    "url": "/static/media/chairs.d2d8f5be.jpg"
+  },
+  {
+    "revision": "1f5b3198c0fc3b00d26db5c798ebc89f",
+    "url": "/static/media/bbq.1f5b3198.jpg"
+  },
+  {
+    "revision": "123f34a0c92e41434f8023077f3fbc2e",
+    "url": "/static/media/bestmenbestwomen.123f34a0.jpeg"
+  },
+  {
+    "revision": "e56f996aa4d127f51a177af93112572a",
+    "url": "/static/media/casamento-nilzadias.e56f996a.jpg"
+  },
+  {
+    "revision": "51a9ef2717037c48e6ffbd8b5d88b53b",
+    "url": "/static/media/casamento-nilzadias-noivos.51a9ef27.jpg"
+  },
+  {
+    "revision": "d33f7aeb3689f5417aad9260b69ab6f3",
+    "url": "/static/media/garden-wc.d33f7aeb.jpg"
+  },
+  {
+    "revision": "06aadad2f125c7027f88f93de907bdfc",
+    "url": "/static/media/rustico-prof-crop.06aadad2.jpg"
+  },
+  {
+    "revision": "cec6151b8c85032a0fcdc1e1e5ca0e25",
+    "url": "/static/media/jardim-gazebo.cec6151b.jpg"
+  },
+  {
+    "revision": "612edc7df29301cf1754fe2fe94a1d0e",
+    "url": "/static/media/damasdonor.612edc7d.jpg"
+  },
+  {
+    "revision": "83a7d395c7826aa722b2b58097370e06",
+    "url": "/static/media/flower.83a7d395.jpg"
+  },
+  {
+    "revision": "871d184a50aee0ab0ebdb5dc23b4f475",
+    "url": "/static/media/festa2.871d184a.jpg"
+  },
+  {
+    "revision": "412e0a3cbc8113229fc3b94eee382014",
+    "url": "/static/media/20-crop.412e0a3c.jpg"
+  },
+  {
+    "revision": "4daff7c33d88c9af3d07cd4b64d187e6",
+    "url": "/static/media/salaorustico-fora.4daff7c3.jpg"
+  },
+  {
+    "revision": "1b27542cd9a8e68852d0bd5aea02241f",
+    "url": "/static/media/wedding-gazebo.1b27542c.jpg"
+  },
+  {
+    "revision": "9925765391aad00a7a7d40a98047e686",
+    "url": "/static/media/decor.99257653.jpg"
+  },
+  {
+    "revision": "0156e03823973fc741ce9f39bce0e409",
+    "url": "/static/media/empresa.0156e038.jpeg"
+  },
+  {
+    "revision": "b098dceabc8b4bfc945545456bef1110",
+    "url": "/static/media/festa.b098dcea.jpg"
+  },
+  {
+    "revision": "a45348dc9398cb4f6066daee069bbeed",
+    "url": "/static/media/wedding-ceremony.a45348dc.jpeg"
+  },
+  {
+    "revision": "123613115876df34c310d6c4ffaee61b",
+    "url": "/static/media/aula.12361311.jpg"
+  },
+  {
+    "revision": "9fd24de98ef2006ebe49e336a7227a72",
+    "url": "/static/media/salaodecorado.9fd24de9.jpg"
+  },
+  {
+    "revision": "eceb85d23c8189b1556b0d3f6e2e1468",
+    "url": "/static/media/salao-dentro.eceb85d2.jpg"
+  },
+  {
+    "revision": "4cb00c1bf1677b112af78a0dcd4c52c6",
+    "url": "/static/media/rustico.4cb00c1b.jpg"
+  },
+  {
+    "revision": "a1c2bcee005944991654f69533d82faa",
+    "url": "/static/media/gazebo-crop.a1c2bcee.jpg"
+  },
+  {
+    "revision": "bd71b826061a4ebd89c1fde0627d7399",
+    "url": "/static/media/piscinas-sm-crop.bd71b826.jpg"
+  },
+  {
+    "revision": "a35ae659602e21fe7d28477712ed9a7a",
+    "url": "/static/media/salaconf-dentro.a35ae659.jpg"
+  },
+  {
+    "revision": "7f6d2fa962c741e240054b0000436502",
+    "url": "/static/media/salaconf-fora.7f6d2fa9.jpg"
+  },
+  {
+    "revision": "5324c53ae480d501e432ced848a2cd7b",
+    "url": "/static/media/rustico-fora.5324c53a.jpg"
+  },
+  {
+    "revision": "77096dd5cca3fcf994d26e3bf62fb393",
+    "url": "/static/media/pavinfant.77096dd5.jpg"
+  },
+  {
+    "revision": "e5b33d06f39320293e4c8c2fcb27ad0e",
+    "url": "/static/media/festivo.e5b33d06.jpeg"
+  },
+  {
+    "revision": "ec78de9214edc7b5d6b15fe71f93e197",
+    "url": "/static/media/top-acontece.ec78de92.jpg"
+  },
+  {
+    "revision": "f89ef93c10721932ad6f3dc5521c4b03",
+    "url": "/static/media/nydiscount-instagram.f89ef93c.jpg"
+  },
+  {
+    "revision": "a27f68cb1674504f2bf6fee776da3c97",
+    "url": "/static/media/newyear.a27f68cb.jpg"
+  },
+  {
+    "revision": "62dc40f3da88d2b5e69211162bb619bc",
+    "url": "/static/media/casamento-mesas-planos.62dc40f3.jpg"
+  },
+  {
+    "revision": "95796cb07f4253f4498cb8c4c3e486fa",
+    "url": "/static/media/decor-planos.95796cb0.jpg"
+  },
+  {
+    "revision": "c0e7dce3dbcb0be5f58e133c5bd26245",
+    "url": "/static/media/cake-plans.c0e7dce3.jpg"
+  },
+  {
+    "revision": "a2ce02b4483cb6a773f0622a64458293",
+    "url": "/static/media/casamento-mesas.a2ce02b4.jpeg"
+  },
+  {
+    "revision": "312dca1ad39293c65789c268f2cb58ea",
+    "url": "/static/media/jp.312dca1a.png"
+  },
+  {
+    "revision": "efc2793c2cb6e26439ea14a52ea18cf5",
+    "url": "/static/media/logo-acontece.efc2793c.png"
+  },
+  {
+    "revision": "07eba14cca7b2872e4ee1052dee89328",
+    "url": "/index.html"
+  }
+];
